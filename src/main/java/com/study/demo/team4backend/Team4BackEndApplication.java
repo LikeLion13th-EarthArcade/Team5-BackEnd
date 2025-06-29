d63e9dbe96cb818da51d0d08ab26e20826e51593
seo hyun seok <tjgustjr33@naver.com>
2025-06-29 20:13:31 +0900

    🎉Begin a Project

---
+++ src/main/java/com/study/demo/team4backend/Team4BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.study.demo.team4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team4BackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/demo/team4backend/Team4BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.demo.team4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
