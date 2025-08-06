3de9058554ca05dedadce3e10e00b64c89c807a6
seohyunseok <tjgustjr33@gmail.com>
2025-08-07 04:03:22 +0900

    ✨feat: Redis, S3 config

---
+++ src/main/java/com/project/team5backend/global/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.project.team5backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // Redis 접속 정보 설정
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        // 기본값은 localhost:6379 (도커 쓰면 host.docker.internal 또는 redis 이름일 수도 있음)
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    // RedisTemplate 설정
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // Key와 Value는 모두 String 기반 (필요시 JSON Serializer로 교체 가능)
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setEnableTransactionSupport(true); // 트랜잭션 사용 여부
+
+        return redisTemplate;
+    }
+}
