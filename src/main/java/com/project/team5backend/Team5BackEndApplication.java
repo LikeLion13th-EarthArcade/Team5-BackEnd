6a9a9cfd80dd935125e5541ecada9c3cd50b0679
seohyunseok <tjgustjr33@gmail.com>
2025-08-20 14:59:57 +0900

    ✨feat: yml 파일 등록

--- src/main/java/com/project/team5backend/Team5BackEndApplication.java
+++ src/main/java/com/project/team5backend/Team5BackEndApplication.java
@@ -2,15 +2,14 @@ package com.project.team5backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class Team4BackEndApplication {
+public class Team5BackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Team4BackEndApplication.class, args);
+        SpringApplication.run(Team5BackEndApplication.class, args);
     }
 
 }
