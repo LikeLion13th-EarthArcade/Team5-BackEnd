db7d15c519d308e13515d442e6fbe1b9b19e2d6b
seoaaa <seoaa3331@gmail.com>
2025-08-23 00:51:32 +0900

    ♻️refactor : 세션 중복 수정3

--- src/main/java/com/project/team5backend/domain/auth/service/command/AuthCommandServiceImpl.java
+++ src/main/java/com/project/team5backend/domain/auth/service/command/AuthCommandServiceImpl.java
@@ -114,22 +114,18 @@ public class AuthCommandServiceImpl implements AuthCommandService {
         Authentication authRequest =
                 new UsernamePasswordAuthenticationToken(request.email(), request.password());
         Authentication authentication = authenticationManager.authenticate(authRequest);
-
         // 2. SecurityContext 생성 및 세션에 저장
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         context.setAuthentication(authentication);
         SecurityContextHolder.setContext(context);
-
-        // 기존 세션 무효화 (중복 로그인 방지)
         HttpSession oldSession = httpRequest.getSession(false);
         if (oldSession != null) oldSession.invalidate();
-
         // 새 세션 생성
         HttpSession session = httpRequest.getSession(true);
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, context);
         session.setMaxInactiveInterval(30 * 60); // 30분
 
-        // 3. 로그인 결과 반환
+        //  로그인 결과 반환
         CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
         User user = userRepository.findById(principal.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("유저 조회 실패"));
